06e9924c49f4cb6cf367e0d46f7a3669c5484a27
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-11-21 17:50:10 -0600

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,54 @@
+/*
+Author: Travis Rosen
+Date: 11/21/2021
+Title: app.js
+Description: server file
+*/
+
+/**
+ * Require statements
+ */
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+
+/**
+ * App configurations
+ */
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended': true}));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../dist/bcrs')));
+app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
+
+//Routes for API's go here
+
+
+
+// Database Connection String
+const conn = '';
+
+/**
+ * Database connection
+ */
+mongoose.connect(conn, {
+  promiseLibrary: require('bluebird'),
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => {
+  console.debug(`Connection to the database instance was successful`);
+}).catch(err => {
+  console.log(`MongoDB Error: ${err.message}`)
+}); // end mongoose connection
+
+
+/**
+ * Create and start server
+ */
+http.createServer(app).listen(port, function() {
+  console.log(`Application started and listening on port: ${port}`)
+}); // end http create server function
