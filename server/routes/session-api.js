8a5e04fe30bb3aa33b53220eefc518c152028870
Abdelmalak <marksol36@gmail.com>
2021-12-01 18:31:46 -0500

    Adding api/session/register, and api/session/verfiy/users/:username

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -67,4 +67,102 @@
      }
 
  });
+
+ /**
+ * Sprint 2 
+ * API:  Register
+ **/
+
+router.post('/register', async(req, res) => {
+    try {
+        User.findOne({ 'userName': req.body.userName }, function(err, user)
+
+            {
+                if (err) {
+                    console.log(err);
+                    const registerUserMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+                    res.status(500).send(registerUserMongodbErrorResponse.toObject());
+
+                } else {
+                    if (!user) {
+
+                        let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds); // salt/hash the password
+                        standardRole = {
+                            role: "standard",
+                        }
+                
+                        // user object 
+                        let registeredUser = {
+                            userName: req.body.userName,
+                            password: hashedPassword,
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            phoneNumber: req.body.phoneNumber,
+                            address: req.body.address,
+                            email: req.body.email,
+                            role: standardRole,
+                            selectedSecurityQuestions: req.body.selectedSecurityQuestions
+                        };
+
+                        User.create(registeredUser, function(err, newUser)
+
+                            {
+                                if (err) {
+                                    console.log(err);
+                                    const newUserMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+                                    res.status(500).send(newUserMongodbErrorResponse.toObject());
+                                } else {
+
+                                    console.log(newUser);
+                                    const registeredUserResponse = new BaseResponse('200', 'Registration successful', newUser);
+                                    res.json(registeredUserResponse.toObject());
+                                }
+                            })
+                    } else {
+                        console.log(`Username ${req.body.userName} already exists.`);
+                        const userExistsError = new BaseResponse('400', `The username '${req.body.userName}' is already in exists.`, null);
+                        res.status(400).send(userExistsError.toObject());
+                    }
+                }
+            })
+    } catch (e) {
+        console.log(e);
+        const registerUserCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+        res.status(500).send(registerUserCatchErrorResponse.toObject());
+    }
+});
+
+/**
+ * Sprint 2
+ * API: Verify User
+ */
+router.get('/verify/users/:userName', async(req, res) => {
+    try {
+        User.findOne({ 'userName': req.params.userName }, function(err, user) {
+
+            if (user) {
+                if (err)
+                {
+                  console.log(err);
+                  const verifyUserMongodbErrorResponse = new ErrorResponse('500', "Internal server error", err );
+                  res.status(500).send(verifyUserMongodbErrorResponse.toObject());
+                }    
+               else {
+                 console.log(user);
+                 const verifyUserResponse = new BaseResponse('200', 'User verification successful', user);
+                 res.json(verifyUserResponse.toObject());
+               }
+              } else {
+                     const invalidUsernameResponse = new BaseResponse('400,', "Invalid username", req.params.userName);
+                   res.status(400).send(invalidUsernameResponse.toObject());
+                    }
+                    })
+            }
+    catch (e) {
+        console.log(e);
+        const verifyUserCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+        res.status(500).send(verifyUserCatchErrorResponse.toObject());
+    }
+});
+
  module.exports = router;
